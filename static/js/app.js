147668ff01510ba23779c17897e99507911f7132
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-03-30 00:40:17 +0530

    added one time messages

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,13 @@ dropdown_toggle.addEventListener('click', ()=>{
   console.log('Element Clicked')
   dropdown_menu.classList.toggle('active')
 })
+
+
+// hide message box after
+setTimeout(() => {
+  const otm_box = document.getElementById('message-box')
+
+  // 👇️ removes element from DOM
+  otm_box.style.display = 'none';
+
+}, 5000)
